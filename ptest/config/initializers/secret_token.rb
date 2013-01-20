282892c5dbd7bc5c6b2153f6a6d8abd9a7c8d302
horus <smonsalve@gmail.com>
2013-01-20 15:28:55 -0500

    sass example project

---
+++ ptest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ptest::Application.config.secret_token = '0f48ce0df2557252828a5ef335717e307fdb15ad2a6488cff4b7da9a38530b7fabc99d9713a560d505bb4fa9340549e8f6ed322be706e7e489468c72d14949eb'
