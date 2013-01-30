825cdf785d85cbd029a6e1645f2b5464420e7253
horus <smonsalve@gmail.com>
2013-01-30 10:57:07 -0500

    file upload test project with CarrierWave, Fog, and s3

--- filetest/config/initializers/carrierwave.rb
+++ filetest/config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+ # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format
 # (all these examples are active by default):
@@ -13,3 +13,17 @@
 # ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :hosts                  => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = ENV["AWS_S3_BUCKET"]                     # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
